30450403c58bc3b67323b8088e59f9aa37aa1476
Adrian Cituk <adrian.citukrosado@cbtis72.edu.mx>
2023-11-20 14:42:06 -0500

    Primer commit

---
+++ src/main/java/net/adriancituk/joseaguilar/controller/HomeController.java
@@ -0,0 +1,15 @@
+package net.adriancituk.joseaguilar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String mostrarHome(Model model) {
+	
+	
+		return"home";
+	}
+}
---
+++ src/test/java/net/adriancituk/joseaguilar/FacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.adriancituk.joseaguilar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
